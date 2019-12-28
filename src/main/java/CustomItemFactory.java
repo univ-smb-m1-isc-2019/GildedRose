676d96985352a0cf55138ca2ab248bf82246a6f7
Baptiste <baptiste.diot@hotmail.fr>
2019-12-28 18:57:56 +0100

    Created CustomItemFactory Class

---
+++ src/main/java/CustomItemFactory.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomItemFactory {
+
+    private final static String AGED_BRIE_ITEM = "Aged Brie";
+    private final static String BACKSTAGE_PASSES_ITEM = "Backstage passes";
+    private final static String CONJURED_ITEM = "Conjured";
+    private final static String SULFURAS_ITEM = "Sulfuras, Hand of Ragnaros";
+
+    private Map<String, CustomItem> itemTypeList;
+
+    public CustomItemFactory(Item item){
+        itemTypeList = new HashMap<>();
+
+        itemTypeList.put(AGED_BRIE_ITEM, new AgedBrie(item));
+        itemTypeList.put(BACKSTAGE_PASSES_ITEM, new BackstagePasses(item));
+        itemTypeList.put(CONJURED_ITEM, new ConjuredItem(item));
+        itemTypeList.put(SULFURAS_ITEM, new Sulfuras(item));
+    }
+}
