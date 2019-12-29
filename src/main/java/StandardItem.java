2d9ee2b4937cc5183e93bdb97a12d59994d2f95f
Baptiste <baptiste.diot@hotmail.fr>
2019-12-29 14:03:30 +0100

    Added code to updateItem in StandardItem

--- src/main/java/StandardItem.java
+++ src/main/java/StandardItem.java
@@ -1,11 +1,25 @@
 public class StandardItem extends CustomItem{
 
+    private static final int QUALITY_EVOLUTION_BEFORE_SELL_DATE = 1;
+    private static final int QUALITY_EVOLUTION_AFTER_SELL_DATE = 2;
+
     public StandardItem(Item item){
         super(item);
     }
 
     @Override
     public void updateItem() {
+        item.sellIn -= 1;
+        if(!hasReachedLowestQualityPossible()) {
 
+            if(!hasSellDatePassed()){
+                item.quality -= QUALITY_EVOLUTION_BEFORE_SELL_DATE;
+            }
+            else{
+                item.quality -= QUALITY_EVOLUTION_AFTER_SELL_DATE;
+            }
+
+            checkQualityLimitReached();
+        }
     }
 }
