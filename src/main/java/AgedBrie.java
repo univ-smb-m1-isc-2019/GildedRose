a97c2a636929d75691bbc4e66307515b4e47f8ad
Baptiste <baptiste.diot@hotmail.fr>
2019-12-29 14:02:31 +0100

    Added code to updateItem in AgedBrie

--- src/main/java/AgedBrie.java
+++ src/main/java/AgedBrie.java
@@ -1,11 +1,25 @@
 public class AgedBrie extends CustomItem{
 
+    private static final int QUALITY_EVOLUTION_BEFORE_SELL_DATE = 1;
+    private static final int QUALITY_EVOLUTION_AFTER_SELL_DATE = 2;
+
     public AgedBrie(Item item){
         super(item);
     }
 
     @Override
     public void updateItem() {
+        item.sellIn -= 1;
+        if(!hasReachedHighestQualityPossible()) {
 
+            if(!hasSellDatePassed()){
+                item.quality += QUALITY_EVOLUTION_BEFORE_SELL_DATE;
+            }
+            else{
+                item.quality += QUALITY_EVOLUTION_AFTER_SELL_DATE;
+            }
+
+            checkQualityLimitReached();
+        }
     }
 }
