5747c30ca7b1761e9b24790e87a950d7c8fe041a
Baptiste <baptiste.diot@hotmail.fr>
2019-12-29 14:02:47 +0100

    Added code to updateItem in BackstagePasses

--- src/main/java/BackstagePasses.java
+++ src/main/java/BackstagePasses.java
@@ -1,11 +1,22 @@
 public class BackstagePasses extends CustomItem{
 
+    private static final int CLOSE_CONCERT_DATE = 10;
+    private static final int IMMINENT_CONCERT_DATE = 5;
+
     public BackstagePasses(Item item){
         super(item);
     }
 
     @Override
     public void updateItem() {
+        item.quality += 1;
+        if(item.sellIn <= CLOSE_CONCERT_DATE) item.quality += 1;
+        if(item.sellIn <= IMMINENT_CONCERT_DATE) item.quality += 1;
 
+        item.sellIn -= 1;
+
+        if(hasSellDatePassed()) item.quality = GildedRose.LOWEST_QUALITY_POSSIBLE;
+
+        checkQualityLimitReached();
     }
 }
