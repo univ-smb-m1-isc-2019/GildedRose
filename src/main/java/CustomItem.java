152d6c9d259b3d8d63059b7d323545f5b351c59f
Baptiste <baptiste.diot@hotmail.fr>
2019-12-29 13:10:00 +0100

    Added methods to test the sellIn date and the quality of the item

--- src/main/java/CustomItem.java
+++ src/main/java/CustomItem.java
@@ -7,4 +7,16 @@ public abstract class CustomItem {
     }
 
     public abstract void updateItem();
+
+    protected boolean hasSellDatePassed(){
+        return item.sellIn < 0;
+    }
+
+    protected boolean hasHighestQualityPossible(){
+        return item.quality > GildedRose.HIGHEST_QUALITY_POSSIBLE;
+    }
+
+    protected boolean hasLowestQualityPossible(){
+        return item.quality < GildedRose.LOWEST_QUALITY_POSSIBLE;
+    }
 }
