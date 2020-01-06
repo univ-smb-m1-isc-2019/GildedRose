28f593b9bad04192331369c54f417e680a8237f1
YannDubost <35998187+YannDubost@users.noreply.github.com>
2020-01-06 16:26:00 +0100

    Test pour le brie qui augmente en qualite

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -2,15 +2,37 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.Optional;
+
 
 public class GildedRoseTest {
 
 	@Test
-	public void sulfura_should_have_the_same_quality() {
+	public void sulfura_should_have_the_same_quality()
+	{
 	    GildedRose gildedRose = new GildedRose();
 	    Item sulfura = gildedRose.get("Sulfuras, Hand of Ragnaros");
 	    int quality = sulfura.getQuality();
-	    gildedRose.updateQuality();
-	    assertTrue(sulfura.getQuality() == quality);
+	    for(int i = 0; i<100; i++)
+	    {
+			gildedRose.updateQuality();
+			assertEquals(quality,sulfura.getQuality());
+		}
+
 	}
+
+	@Test
+	public void brie_should_increase_quality()
+	{
+		GildedRose gildedRose = new GildedRose();
+		Item brie = gildedRose.get("Aged Brie");
+		int quality = brie.getQuality();
+		for(int i = 0; i<100; i++)
+		{
+			gildedRose.updateQuality();
+			assertTrue(quality <= brie.getQuality());
+		}
+	}
+
+
 }
