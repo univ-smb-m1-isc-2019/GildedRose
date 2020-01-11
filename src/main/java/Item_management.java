5edbf3bf9aa998a354d4846020b6f7f10faa7515
Aramais <grunge.sektor@gmail.com>
2020-01-11 23:36:25 +0100

    Added isLegendaryItem method. Modified qualityIncrementBackstage, itemQualityDecrement. Corrected dayPassed. Deleted qualityLessThan50.

--- src/main/java/Item_management.java
+++ src/main/java/Item_management.java
@@ -4,36 +4,32 @@ public class Item_management {
     public static int QUALITY_MIN = 0;
     private static String LEGENDARY_ITEM = "Sulfuras, Hand of Ragnaros";
 
-    public boolean qualityLessThan50 (Item myItem){
-       return myItem.getQuality() < QUALITY_MAX;
+    public boolean isLegendaryItem(Item myItem){
+        return LEGENDARY_ITEM.equals(myItem.getName());
     }
 
     public void itemQualityIncrement(Item myItem){
-        if (myItem.getQuality() > QUALITY_MIN)
+        if (myItem.getQuality() < QUALITY_MAX)
             myItem.setQuality(myItem.getQuality() + 1);
     }
 
     public void itemQualityDecrement(Item myItem){
-        if (myItem.getQuality() > QUALITY_MIN)
+        if (myItem.getQuality() > QUALITY_MIN && !isLegendaryItem(myItem))
             myItem.setQuality(myItem.getQuality() - 1);
     }
 
     public void dayPassed(Item myItem){
-            if (!LEGENDARY_ITEM.equals(myItem.getName()))
-                itemQualityDecrement(myItem);
+        if (!isLegendaryItem(myItem))
+            myItem.setSellIn(myItem.getSellIn() - 1);
     }
 
     public void qualityIncrementBackstage(Item myItem){
 
         if (myItem.getSellIn() < 11)
-            if (qualityLessThan50(myItem)) {
-                itemQualityIncrement(myItem);
-            }
+            itemQualityIncrement(myItem);
 
         if (myItem.getSellIn() < 6)
-            if (qualityLessThan50(myItem)) {
                 itemQualityIncrement(myItem);
-            }
     }
 
 
