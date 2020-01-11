dc126c0affef4ac6cb4713bc24e78c2dda7349ec
Aramais <grunge.sektor@gmail.com>
2020-01-12 00:18:57 +0100

    Added call to method createList.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         CreateItemList myItemList = new CreateItemList();
+        myItemList.createList();
+
         GildedRose gRose = new GildedRose(myItemList.getItems());
         gRose.updateQuality();
     }
