ba6cf0c9e18c343b311f7f752d70677a70c040d8
Aramais <grunge.sektor@gmail.com>
2020-01-11 21:51:14 +0100

    'Main' update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+        CreateItemList myItemList = new CreateItemList();
+        new GildedRose(myItemList.getItems());
 
     }
 
