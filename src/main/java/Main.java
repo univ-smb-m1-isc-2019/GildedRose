086b540fb4a7cf89960b1e8275889d5633e8de47
Aramais <grunge.sektor@gmail.com>
2020-01-12 00:00:33 +0100

    Added call to method updateQuality.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         CreateItemList myItemList = new CreateItemList();
-        new GildedRose(myItemList.getItems());
-
+        GildedRose gRose = new GildedRose(myItemList.getItems());
+        gRose.updateQuality();
     }
 
 }
