ad9252aa43452fb3c4aa707538ed78bd7f4cb96c
Aramais <grunge.sektor@gmail.com>
2020-01-16 22:56:40 +0100

    Added methods which create list of other possibility of items. CreateItemResultsList_2(),CreateItemResultsList_2().
    runTest_2(), testListsEquals_2() were added.

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -11,9 +11,11 @@ public class GildedRoseTest {
 	private static GildedRose gRoseTest;
 	private static List<Item> items;
 	private static List<Item> itemsExpectedResult;
+	private static List<Item> items2;
+	private static List<Item> itemsExpectedResult2;
 
 	/** Initialization list of items **/
-	public static void CreateItemList() {
+	public static void CreateItemList_1() {
 		items = new ArrayList<Item>();
 		items.add(new Item("+5 Dexterity Vest", 10, 20));
 		items.add(new Item("Aged Brie", 2, 0));
@@ -24,7 +26,7 @@ public class GildedRoseTest {
 
 
 	/** Initialization list of expected items **/
-	public static void CreateItemResultsList() {
+	public static void CreateItemResultsList_1() {
 		itemsExpectedResult = new ArrayList<Item>();
 		itemsExpectedResult.add(new Item("+5 Dexterity Vest", 9, 19));
 		itemsExpectedResult.add(new Item("Aged Brie", 1, 1));
@@ -33,18 +35,59 @@ public class GildedRoseTest {
 		itemsExpectedResult.add(new Item("Backstage passes to a TAFKAL80ETC concert", 14, 21));
 	}
 
+	/** Initialization list of items2 **/
+	public static void CreateItemList_2() {
+		items2 = new ArrayList<Item>();
+		items2.add(new Item("+5 Dexterity Vest", 0, 20));
+		items2.add(new Item("Aged Brie", 0, 0));
+		items2.add(new Item("Elixir of the Mongoose", 0, 7));
+		items2.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+		items2.add(new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20));
+	}
+
+
+	/** Initialization list of expected items2 **/
+	public static void CreateItemResultsList_2() {
+		itemsExpectedResult2 = new ArrayList<Item>();
+		itemsExpectedResult2.add(new Item("+5 Dexterity Vest", -1, 18));
+		itemsExpectedResult2.add(new Item("Aged Brie", -1, 2));
+		itemsExpectedResult2.add(new Item("Elixir of the Mongoose", -1, 5));
+		itemsExpectedResult2.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+		itemsExpectedResult2.add(new Item("Backstage passes to a TAFKAL80ETC concert", -1, 0));
+	}
+
+
+
 	@BeforeClass
-	public static void runTest(){
-		CreateItemList();
-		CreateItemResultsList();
+	public static void runTest_1(){
+		CreateItemList_1();
+		CreateItemResultsList_1();
 		gRoseTest = new GildedRose(items);
 		gRoseTest.updateQuality();
 	}
 
+	@BeforeClass
+	public static void runTest_2(){
+		CreateItemList_2();
+		CreateItemResultsList_2();
+		gRoseTest = new GildedRose(items2);
+		gRoseTest.updateQuality();
+	}
+
 	@Test
-	public void testListsEquals(){
+	public void testListsEquals_1(){
+		System.out.println("List 1");
 		for(int i = 0; i < items.size(); i++){
 			updateQualityStandardItem(items.get(i),itemsExpectedResult.get(i));
+			System.out.println(i + ": Item is OK");
+		}
+	}
+
+	@Test
+	public void testListsEquals_2(){
+		System.out.println("\nList 2");
+		for(int i = 0; i < items.size(); i++){
+			updateQualityStandardItem(items2.get(i),itemsExpectedResult2.get(i));
 			System.out.println(i + ": Item is OK");
 		}
 	}
