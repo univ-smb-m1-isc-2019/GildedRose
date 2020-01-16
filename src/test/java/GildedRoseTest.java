c28507b408298d16823952b734912090565a618a
Aramais <grunge.sektor@gmail.com>
2020-01-16 21:26:33 +0100

    Method runTest() is created.

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,12 +1,26 @@
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 
 public class GildedRoseTest {
 
-	@Test
-	public void testTheTruth() {
-		assertTrue(true);
+	private static GildedRose gRoseTest;
+
+	@BeforeClass
+	public static void runTest(){
+		CreateItemList listForTests = new CreateItemList();
+		gRoseTest = new GildedRose(listForTests.getItems());
 	}
+
+	@Test
+	public void updateQuality(){
+
+	}
+
+
+
+
+
 }
