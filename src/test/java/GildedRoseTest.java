ab64e96bbd3936d5b6ac2b5b95ad661b51ce65f8
Aramais <grunge.sektor@gmail.com>
2020-01-16 22:26:48 +0100

    Methods for tests testListsEquals() and updateQualityStandardItem() were implemented.

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -25,11 +26,11 @@ public class GildedRoseTest {
 	/** Initialization list of expected items **/
 	public static void CreateItemResultsList() {
 		itemsExpectedResult = new ArrayList<Item>();
-		itemsExpectedResult.add(new Item("+5 Dexterity Vest", 10, 20));
-		itemsExpectedResult.add(new Item("Aged Brie", 2, 0));
-		itemsExpectedResult.add(new Item("Elixir of the Mongoose", 5, 7));
+		itemsExpectedResult.add(new Item("+5 Dexterity Vest", 9, 19));
+		itemsExpectedResult.add(new Item("Aged Brie", 1, 1));
+		itemsExpectedResult.add(new Item("Elixir of the Mongoose", 4, 6));
 		itemsExpectedResult.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-		itemsExpectedResult.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+		itemsExpectedResult.add(new Item("Backstage passes to a TAFKAL80ETC concert", 14, 21));
 	}
 
 	@BeforeClass
@@ -37,11 +38,21 @@ public class GildedRoseTest {
 		CreateItemList();
 		CreateItemResultsList();
 		gRoseTest = new GildedRose(items);
+		gRoseTest.updateQuality();
 	}
 
 	@Test
-	public void updateQualityStandardItem(Item item, Item itemResultExpected){
+	public void testListsEquals(){
+		for(int i = 0; i < items.size(); i++){
+			updateQualityStandardItem(items.get(i),itemsExpectedResult.get(i));
+			System.out.println(i + ": Item is OK");
+		}
+	}
 
+
+	public void updateQualityStandardItem(Item item, Item itemResultExpected){
+		Assert.assertEquals(itemResultExpected.getQuality(), item.getQuality());
+		Assert.assertEquals(itemResultExpected.getSellIn(),item.getSellIn());
 	}
 
 
