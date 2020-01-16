af8a6329feff5ff32976ce4d00d5594fcaaae3ab
Baptiste <baptiste.diot@hotmail.fr>
2020-01-16 17:29:54 +0100

    Added test updateQuality for standardItem when sellIn superior than 0

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -9,6 +9,22 @@ import static org.assertj.core.api.Assertions.*;
 public class GildedRoseTest {
 
 	@Test
+	public void standardItemQualityShouldDecreaseByOneWhenSellInSuperiorThanZero(){
+		int QUALITY_DECREASE_RATE = 1;
+
+		GildedRose inn = new GildedRose();
+		Item standardItem = inn.getItem("+5 Dexterity Vest");
+
+		int qualityBeforeUpdate;
+		while(standardItem.sellIn > 0){
+			qualityBeforeUpdate = standardItem.quality;
+
+			inn.updateQuality();
+			assertThat(standardItem.quality).isEqualTo(qualityBeforeUpdate - QUALITY_DECREASE_RATE);
+		}
+	}
+
+	@Test
 	public void sulfurasQualityShouldNeverChange(){
 		GildedRose inn = new GildedRose();
 
