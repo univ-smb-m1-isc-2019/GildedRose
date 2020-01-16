c5bcd2fd9b5b4bdb42206c05470ba3c1ef4fc39b
Aramais <grunge.sektor@gmail.com>
2020-01-16 20:28:56 +0100

    Call to method createList() is deleted.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ public class Main {
 
     public static void main(String[] args) {
         CreateItemList myItemList = new CreateItemList();
-        myItemList.createList();
 
         GildedRose gRose = new GildedRose(myItemList.getItems());
         gRose.updateQuality();
