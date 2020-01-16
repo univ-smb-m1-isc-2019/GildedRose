2b3dcdb1c8e72e4824055a8a609a31e03732d255
ThomasBottollier <Thomas.Bottollier-Darbelin@etu.univ-savoie.fr>
2020-01-16 21:03:35 +0100

    Created itemsInit method

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -6,13 +6,14 @@ public class GildedRose {
 
 	private static List<Item> items = null;
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		
         System.out.println("OMGHAI!");
-		
+        itemsInit();
+        updateQuality();
+
+}
+
+    public static void itemsInit(){
         items = new ArrayList<Item>();
         items.add(new Item("+5 Dexterity Vest", 10, 20));
         items.add(new Item("Aged Brie", 2, 0));
@@ -20,12 +21,8 @@ public class GildedRose {
         items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
         items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
         items.add(new Item("Conjured Mana Cake", 3, 6));
+    }
 
-        updateQuality();
-}
-
-
-	
     public static void updateQuality()
     {
         for (int i = 0; i < items.size(); i++)
