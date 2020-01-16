0358e3b8af85c05a8dc9a2812a49f192e3665313
YannDubost <35998187+YannDubost@users.noreply.github.com>
2020-01-16 19:34:46 +0100

    ajout de la valeur minimal pour les items commun

--- src/main/java/CommonItem.java
+++ src/main/java/CommonItem.java
@@ -1,7 +1,36 @@
 public class CommonItem extends Item {
+    private int minimalValue;
 
+    public CommonItem(String name, int sellIn, int quality)
+    {
 
-    public CommonItem(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
+        setMinimalValue(0);
     }
+
+    public void updateQuality()
+    {
+        if (getItemQuality() > getMinimalValue()) {
+
+
+        }
+    }
+
+    public int getItemQuality()
+    {
+        return this.quality;
+    }
+
+    public void setMinimalValue(int min)
+    {
+        this.minimalValue = min;
+    }
+
+    public int getMinimalValue()
+    {
+        return minimalValue;
+    }
+
+
+
 }
--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 
