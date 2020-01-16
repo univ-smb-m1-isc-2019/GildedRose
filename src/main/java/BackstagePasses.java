114db49483aab39d09fbc6f427b32a0fe211d7ac
YannDubost <35998187+YannDubost@users.noreply.github.com>
2020-01-16 20:05:32 +0100

    Ajout des valeur qui determine le moment ou la quality augmente par 2 et par 3

--- src/main/java/BackstagePasses.java
+++ src/main/java/BackstagePasses.java
@@ -1,9 +1,14 @@
 public class BackstagePasses extends improveItem {
-    int maximalValue;
+    private int maximalValue;
+    private int increaseByTwo;
+    private int increaseByThree;
 
     public BackstagePasses(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
         setMaximalValue(50);
+        setIncreaseByTwo(11);
+        setIncreaseByThree(6);
+
     }
 
     @Override
@@ -30,6 +35,24 @@ public class BackstagePasses extends improveItem {
     public void setMaximalValue(int value)
     {
         this.maximalValue = value;
+    }
+    
+    public void setIncreaseByTwo(int value)
+    {
+        increaseByTwo = value;
+    }
+    public void setIncreaseByThree(int value)
+    {
+        increaseByThree = value;
+    }
+
+    public int getIncreaseByTwo()
+    {
+        return increaseByTwo;
+    }
+    public int getIncreaseByThree()
+    {
+        return increaseByThree;
     }
 
 }
