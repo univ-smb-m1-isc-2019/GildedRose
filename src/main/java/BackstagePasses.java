e82c5ce6da5d06e353eae29d9962dd8971abf7c1
YannDubost <35998187+YannDubost@users.noreply.github.com>
2020-01-16 20:04:01 +0100

    Ajout du premier increment de la quality du passe

--- src/main/java/BackstagePasses.java
+++ src/main/java/BackstagePasses.java
@@ -1,6 +1,9 @@
 public class BackstagePasses extends improveItem {
+    int maximalValue;
+
     public BackstagePasses(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
+        setMaximalValue(50);
     }
 
     @Override
@@ -9,6 +12,24 @@ public class BackstagePasses extends improveItem {
     }
     private void decrementItemSellIn() {
         setSellIn(getSellIn()-1);
+    }
+
+    @Override
+    protected void updateQuality() {
+        if (getQuality() < getMaximalValue()) {
+            incrementItemQuality();
+        }
+    }
+
+    public void incrementItemQuality()
+    {
+        setQuality(getQuality() + 1);
+    }
+    public  int getMaximalValue(){ return maximalValue; }
+
+    public void setMaximalValue(int value)
+    {
+        this.maximalValue = value;
     }
 
 }
