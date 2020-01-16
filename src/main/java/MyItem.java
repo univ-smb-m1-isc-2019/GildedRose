dd4d3b9336d4fa9c498760bf2ee2f297fa175eb1
YannDubost <35998187+YannDubost@users.noreply.github.com>
2020-01-16 20:31:16 +0100

    Ajout des informations de myitem

--- src/main/java/MyItem.java
+++ src/main/java/MyItem.java
@@ -1,6 +1,34 @@
 public abstract class MyItem extends Item {
+    private int minimalValue;
+    private int maximalValue;
 
     public MyItem(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
+    }
+
+    protected void updateQuality()
+    {
+
+    }
+    protected void udpdateSellIn()
+    {
+
+    }
+
+    public  int getMaximalValue(){ return maximalValue; }
+
+    public void setMaximalValue(int value)
+    {
+        this.maximalValue = value;
+    }
+
+    public void setMinimalValue(int min)
+    {
+        this.minimalValue = min;
+    }
+
+    public int getMinimalValue()
+    {
+        return minimalValue;
     }
 }
