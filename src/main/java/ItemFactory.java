9e302d7402801d8b7c196fabc515a6bd7fb7dbd8
Alexandre <alascenci@gmail.com>
2020-01-16 20:58:20 +0100

    Optimisation de la méthode getInstance d'ItemFactory - prise en compte des noms proches.

--- src/main/java/ItemFactory.java
+++ src/main/java/ItemFactory.java
@@ -1,6 +1,6 @@
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class ItemFactory {
     Map<String, Class<? extends AbstractItem>> instances;
@@ -9,15 +9,32 @@ public class ItemFactory {
         this.instances = new HashMap<>();
 
         instances.put("Sulfuras, Hand of Ragnaros", LegendaryItem.class);
-        instances.put("+5 Dexterity Vest", OrdinaryItem.class);
+        instances.put("Dexterity Vest", OrdinaryItem.class);
         instances.put("Elixir of the Mongoose", OrdinaryItem.class);
         instances.put("Aged Brie", AgedBrie.class);
-        instances.put("Backstage passes to a TAFKAL80ETC concert", BackstagePasses.class);
+        instances.put("Backstage passes", BackstagePasses.class);
     }
 
     // return the instance corresponding to the given item.
-    public AbstractItem getInstance(Item item) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        Class<?> c = instances.get(item.name);
-        return (AbstractItem) c.getConstructor(Item.class).newInstance(item);
+    public AbstractItem getInstance(Item item) throws Exception {
+        // deal with stuff like conjured items, that has "conjured" in their name, as an example.
+        Optional<String> similarName = findFirstKeyInString(item.name);
+
+        if(similarName.isPresent()) {
+            return (AbstractItem) instances
+                    .get(similarName.get())
+                    .getConstructor(Item.class)
+                    .newInstance(item);
+        } else {
+            throw new Exception("There is no item called this way in our world! Are you sure you're playing WOW?");
+        }
     }
+
+    private Optional<String> findFirstKeyInString(String str) {
+        return instances.keySet()
+                .stream()
+                .filter(key -> str.contains(key))
+                .findFirst();
+    }
+
 }
