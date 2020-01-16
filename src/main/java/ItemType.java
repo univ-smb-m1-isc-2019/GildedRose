9e4b403b0a121cabc9a1a58bb354bd3cad78376c
ThomasBottollier <Thomas.Bottollier-Darbelin@etu.univ-savoie.fr>
2020-01-16 21:52:49 +0100

    Created ItemType class

---
+++ src/main/java/ItemType.java
@@ -0,0 +1,36 @@
+public class ItemType {
+    public enum Type { STANDARD, CONJURED, LEGENDARY, CHEESE, TICKET };
+    public String name;
+    public int sellIn;
+    public int quality;
+    public Type type;
+
+    public ItemType(Item item) {
+        this.name = item.getName();
+        this.sellIn = item.getSellIn();
+        this.quality = item.getQuality();
+
+        Type itemType;
+        switch(this.name) {
+            case "Conjured Mana Cake":
+                itemType = Type.CONJURED;
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+                itemType = Type.LEGENDARY;
+                break;
+            case "Aged Brie":
+                itemType = Type.CHEESE;
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                itemType = Type.TICKET;
+                break;
+            case "Elixir of the Mongoose":
+            case "+5 Dexterity Vest":
+            default:
+                itemType = Type.STANDARD;
+        }
+
+        this.type = itemType;
+
+    }
+}
