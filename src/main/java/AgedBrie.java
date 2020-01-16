cab59673a60c95eae581aae878b8c1e407f5ebdc
YannDubost <35998187+YannDubost@users.noreply.github.com>
2020-01-16 20:16:02 +0100

    Ajout de la decrementation de la date de sellin du brie

--- src/main/java/AgedBrie.java
+++ src/main/java/AgedBrie.java
@@ -3,4 +3,10 @@ public class AgedBrie extends improveItem {
     public AgedBrie(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
     }
+
+
+    @Override
+    protected void udpdateSellIn() {
+        decrementItemSellIn();
+    }
 }
