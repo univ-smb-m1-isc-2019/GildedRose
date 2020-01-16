fc275bc88559e6a0e3d416dba351b78f3aed4f26
Baptiste <baptiste.diot@hotmail.fr>
2020-01-16 16:58:08 +0100

    Added test for sulfuras Item

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,12 +1,24 @@
+import static java.util.stream.IntStream.range;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
 
 
 public class GildedRoseTest {
-
+	
 	@Test
-	public void testTheTruth() {
-		assertTrue(true);
+	public void sulfurasQualityShouldNeverChange(){
+		GildedRose inn = new GildedRose();
+
+		Item sulfuras = inn.getItem("Sulfuras, Hand of Ragnaros");
+
+		int originalQuality = sulfuras.quality;
+
+		for(int i = 0; i < 100; ++i) {
+			inn.updateQuality();
+			assertThat(sulfuras.quality).isEqualTo(originalQuality);
+		}
 	}
 }
