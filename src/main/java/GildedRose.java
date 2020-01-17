45474dbea2f5c9ce8a95f2eef6344a62fdf996d3
Khelifi Sabrina <sabrina1.1khelifi@gmail.com>
2020-01-17 17:13:51 +0100

    another refactoring

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -95,29 +95,19 @@ public class GildedRose {
 
             if (item.getSellIn() < 0)
             {
-                if (!"Aged Brie".equals(item.getName()))
+                if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
                 {
-                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
+                    if (item.getQuality() > 0)
                     {
-                        if (item.getQuality() > 0)
+                        if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
                         {
-                            if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
-                            {
-                                item.setQuality(item.getQuality() - 1);
-                            }
+                            item.setQuality(item.getQuality() - 1);
                         }
-                    }
-                    else
-                    {
-                        item.setQuality(item.getQuality() - item.getQuality());
                     }
                 }
                 else
                 {
-                    if (item.getQuality() < 50)
-                    {
-                        item.setQuality(item.getQuality() + 1);
-                    }
+                    item.setQuality(0);
                 }
             }
         }
