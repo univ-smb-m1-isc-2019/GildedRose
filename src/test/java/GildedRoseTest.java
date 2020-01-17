47458bb684b4fda017d56e727d6eb3ca84569585
Qin Jia <34362836+QIN-Jia@users.noreply.github.com>
2020-01-17 16:03:24 +0100

    Maj Test

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -2,22 +2,11 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.Date;
+import java.util.List;
+
 
 public class GildedRoseTest {
-    /*
-    @Test
-    public void sulfuras_should_keep_same_quality() {
-        GildedRose inn = new GildedRose();
-        UpdateQuality uq = new UpdateQuality();
-        Item sulfuras = inn.get("Sulfuras,  Hand of Ragnaros");
-        int originQuality = sulfuras.quality;
-
-        for (int i = 0; i < 100; i++) {
-            uq.UpdateQuality(inn.getItemList());
-            assertTrue(originQuality == sulfuras.quality);
-        }
-    }
-    */
 
     @Test
     public void sulfuras_should_keep_same_quality() {
@@ -25,11 +14,31 @@ public class GildedRoseTest {
         UpdateQuality uq = new UpdateQuality();
         Item sulfuras = inn.get("Sulfuras, Hand of Ragnaros");
         int originalQuality = sulfuras.quality;
+        // System.out.println("Original Quality = " + originalQuality);
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             uq.UpdateQuality(inn.getItemList());
             assertTrue(originalQuality == sulfuras.quality);
+            //System.out.println("Day" + i + ": Quality of sulfuras actual => " + sulfuras.quality);
         }
-
     }
+
+    @Test
+    public void quality_never_to_negative() {
+        GildedRose inn = new GildedRose();
+        UpdateQuality uq = new UpdateQuality();
+        List<ResetItem> tmpList = inn.getItemList();
+
+        for (int i = 0; i < 100; i++) {
+            uq.UpdateQuality(tmpList);
+            tmpList.stream()
+                    .forEach(
+                            resetItem -> {
+                                assertTrue(resetItem.getItem().getQuality() >= 0);
+                            }
+                    );
+        }
+    }
+
+    
 }
