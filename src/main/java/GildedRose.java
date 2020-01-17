6bb914388a9781a4ae656513354da5b326ee55ca
Khelifi Sabrina <sabrina1.1khelifi@gmail.com>
2020-01-17 17:42:02 +0100

    another refactoring

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -54,47 +54,7 @@ public class GildedRose {
                 }
             }
         }else{
-            if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
-            {
-                if (item.getQuality() > 0)
-                {
-                    if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
-                    {
-                        item.setQuality(item.getQuality() - 1);
-                    }
-                }
-            }
-            else
-            {
-                if (item.getQuality() < 50)
-                {
-                    item.setQuality(item.getQuality() + 1);
-
-                    if (item.getSellIn() < 11)
-                    {
-                        if (item.getQuality() < 50)
-                        {
-                            item.setQuality(item.getQuality() + 1);
-                        }
-                    }
-
-                    if (item.getSellIn() < 6)
-                    {
-                        if (item.getQuality() < 50)
-                        {
-                            item.setQuality(item.getQuality() + 1);
-                        }
-                    }
-                }
-            }
-
-            if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
-            {
-                item.setSellIn(item.getSellIn() - 1);
-            }
-
-            if (item.getSellIn() < 0)
-            {
+	        if("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())){
                 if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
                 {
                     if (item.getQuality() > 0)
@@ -107,7 +67,106 @@ public class GildedRose {
                 }
                 else
                 {
-                    item.setQuality(0);
+                    if (item.getQuality() < 50)
+                    {
+                        item.setQuality(item.getQuality() + 1);
+
+                        if (item.getSellIn() < 11)
+                        {
+                            if (item.getQuality() < 50)
+                            {
+                                item.setQuality(item.getQuality() + 1);
+                            }
+                        }
+
+                        if (item.getSellIn() < 6)
+                        {
+                            if (item.getQuality() < 50)
+                            {
+                                item.setQuality(item.getQuality() + 1);
+                            }
+                        }
+                    }
+                }
+
+                if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
+                {
+                    item.setSellIn(item.getSellIn() - 1);
+                }
+
+                if (item.getSellIn() < 0)
+                {
+                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
+                    {
+                        if (item.getQuality() > 0)
+                        {
+                            if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
+                            {
+                                item.setQuality(item.getQuality() - 1);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        item.setQuality(0);
+                    }
+                }
+            }else{
+                if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
+                {
+                    if (item.getQuality() > 0)
+                    {
+                        if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
+                        {
+                            item.setQuality(item.getQuality() - 1);
+                        }
+                    }
+                }
+                else
+                {
+                    if (item.getQuality() < 50)
+                    {
+                        item.setQuality(item.getQuality() + 1);
+
+                        if (item.getSellIn() < 11)
+                        {
+                            if (item.getQuality() < 50)
+                            {
+                                item.setQuality(item.getQuality() + 1);
+                            }
+                        }
+
+                        if (item.getSellIn() < 6)
+                        {
+                            if (item.getQuality() < 50)
+                            {
+                                item.setQuality(item.getQuality() + 1);
+                            }
+                        }
+                    }
+                }
+
+                if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
+                {
+                    item.setSellIn(item.getSellIn() - 1);
+                }
+
+                if (item.getSellIn() < 0)
+                {
+                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
+                    {
+                        if (item.getQuality() > 0)
+                        {
+                            if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
+                            {
+                                item.setQuality(item.getQuality() - 1);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        item.setQuality(0);
+                    }
                 }
             }
         }
