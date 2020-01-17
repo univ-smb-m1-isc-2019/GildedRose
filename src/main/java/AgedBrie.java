eaaf697f20151b7eb42ef208b4afc2c21c35bfa9
Khelifi Sabrina <sabrina1.1khelifi@gmail.com>
2020-01-17 18:38:50 +0100

    Replace conditional with Polymorohisme : BackstagePass subclass

--- src/main/java/AgedBrie.java
+++ src/main/java/AgedBrie.java
@@ -1,5 +1,5 @@
 public class AgedBrie extends Item {
-    public AgedBrie(String name, int sellIn, int quality) {
-        super(name, sellIn, quality);
+    public AgedBrie( int sellIn, int quality) {
+        super("Aged Brie", sellIn, quality);
     }
 }
---
+++ src/main/java/BackstagePass.java
@@ -0,0 +1,5 @@
+public class BackstagePass extends Item {
+    public BackstagePass(int sellIn, int quality) {
+        super("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
+    }
+}
