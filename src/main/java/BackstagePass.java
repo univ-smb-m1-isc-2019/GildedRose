0c5e58c946de1c849a58a96cfae1296a1aff4a77
Khelifi Sabrina <sabrina1.1khelifi@gmail.com>
2020-01-17 21:10:15 +0100

    Replace conditional with Polymorohisme : ItemFactory class correction

--- src/main/java/BackstagePass.java
+++ src/main/java/BackstagePass.java
@@ -5,7 +5,26 @@ public class BackstagePass extends ItemFactory{
     }
     @Override
     protected void  doUpdateQuality(){
+        if (quality < 50) {
+            quality = quality + 1;
 
+            if (sellIn < 11) {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+
+            if (sellIn < 6) {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+        }
+        sellIn = sellIn - 1;
+
+        if (sellIn < 0) {
+            quality = 0;
+        }
 
     }
 }
