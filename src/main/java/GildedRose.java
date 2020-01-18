d40fceaca526a464e82866528a31b7f7ff1174bd
Olivier LESAGE <olivier.lesage@etu.univ-savoie>
2020-01-18 19:24:04 +0100

    Ajout du plugin exec-maven-plugin au projet.

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -10,9 +10,9 @@ public class GildedRose {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
         System.out.println("OMGHAI!");
-		
+
         items = new ArrayList<Item>();
         items.add(new Item("+5 Dexterity Vest", 10, 20));
         items.add(new Item("Aged Brie", 2, 0));
@@ -25,12 +25,12 @@ public class GildedRose {
 }
 
 
-	
+
     public static void updateQuality()
     {
         for (int i = 0; i < items.size(); i++)
         {
-            if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) 
+            if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
             {
                 if (items.get(i).getQuality() > 0)
                 {
