9da813fa4f741c2a4b83ef1f8a6cac1f37e2b27f
Olivier LESAGE <olivier.lesage@etu.univ-savoie>
2020-01-18 23:14:38 +0100

    Ajout d'une méthode updateQuality à la classe ItemHolder.

--- src/main/java/ItemHolder.java
+++ src/main/java/ItemHolder.java
@@ -37,4 +37,54 @@ public class ItemHolder {
     public void setQuality(int quality) {
         this.item.setQuality(quality);
     }
+
+    public void updateQuality() {
+        if ((!"Aged Brie".equals(this.getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(this.getName())) {
+            if (this.getQuality() > 0) {
+                if (!"Sulfuras, Hand of Ragnaros".equals(this.getName())) {
+                    this.changeQualityOf(-1);
+                }
+            }
+        } else {
+            if (this.getQuality() < 50) {
+                this.changeQualityOf(1);
+
+                if ("Backstage passes to a TAFKAL80ETC concert".equals(this.getName())) {
+                    if (this.getSellIn() < 11) {
+                        if (this.getQuality() < 50) {
+                            this.changeQualityOf(1);
+                        }
+                    }
+
+                    if (this.getSellIn() < 6) {
+                        if (this.getQuality() < 50) {
+                            this.changeQualityOf(1);
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!"Sulfuras, Hand of Ragnaros".equals(this.getName())) {
+            this.sellInPassed();
+        }
+
+        if (this.getSellIn() < 0) {
+            if (!"Aged Brie".equals(this.getName())) {
+                if (!"Backstage passes to a TAFKAL80ETC concert".equals(this.getName())) {
+                    if (this.getQuality() > 0) {
+                        if (!"Sulfuras, Hand of Ragnaros".equals(this.getName())) {
+                            this.changeQualityOf(-1);
+                        }
+                    }
+                } else {
+                    this.changeQualityOf(this.getQuality());
+                }
+            } else {
+                if (this.getQuality() < 50) {
+                    this.changeQualityOf(1);
+                }
+            }
+        }
+    }
 }
