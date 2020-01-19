d217f56aae45e544012e99ea0d060a019356eb4b
Olivier LESAGE <olivier.lesage@etu.univ-savoie>
2020-01-19 23:31:19 +0100

    Ajout d'objet de type "NormalItem" dans le main.

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -14,9 +14,9 @@ public class GildedRose {
         System.out.println("OMGHAI!");
 
         items = new ArrayList<ItemHolder>();
-        items.add(new ItemHolder(new Item("+5 Dexterity Vest", 10, 20)));
+        items.add(new NormalItem("+5 Dexterity Vest", 10, 20));
         items.add(new ItemHolder(new Item("Aged Brie", 2, 0)));
-        items.add(new ItemHolder(new Item("Elixir of the Mongoose", 5, 7)));
+        items.add(new NormalItem("Elixir of the Mongoose", 5, 7));
         items.add(new ItemHolder(new Item("Sulfuras, Hand of Ragnaros", 0, 80)));
         items.add(new ItemHolder(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)));
         items.add(new ItemHolder(new Item("Conjured Mana Cake", 3, 6)));
