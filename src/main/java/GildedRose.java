e34c244f887e36c18d27269beb94d4d8fe087014
Olivier LESAGE <olivier.lesage@etu.univ-savoie>
2020-01-19 23:58:56 +0100

    Ajout d'objet de type "CollectorItem" dans le main.

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -15,7 +15,7 @@ public class GildedRose {
 
         items = new ArrayList<ItemHolder>();
         items.add(new NormalItem("+5 Dexterity Vest", 10, 20));
-        items.add(new ItemHolder(new Item("Aged Brie", 2, 0)));
+        items.add(new CollectorItem("Aged Brie", 2, 0));
         items.add(new NormalItem("Elixir of the Mongoose", 5, 7));
         items.add(new LegendaryItem("Sulfuras, Hand of Ragnaros", 0, 80));
         items.add(new ItemHolder(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)));
