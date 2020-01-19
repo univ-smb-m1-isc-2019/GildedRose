baf600bf6c36974c19cb54b92a9e07d0e61510b4
Olivier LESAGE <olivier.lesage@etu.univ-savoie>
2020-01-20 00:29:15 +0100

    Ajout d'objet de type "ConjuredItem" dans le main.
    La liste d'item n'est plus une liste d'ItemHolder mais un liste d'ItemInterface.

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class GildedRose {
 
-    private static List<ItemHolder> items = null;
+    private static List<ItemInterface> items = null;
 
     /**
      * @param args
@@ -13,13 +13,13 @@ public class GildedRose {
 
         System.out.println("OMGHAI!");
 
-        items = new ArrayList<ItemHolder>();
+        items = new ArrayList<ItemInterface>();
         items.add(new NormalItem("+5 Dexterity Vest", 10, 20));
         items.add(new CollectorItem("Aged Brie", 2, 0));
         items.add(new NormalItem("Elixir of the Mongoose", 5, 7));
         items.add(new LegendaryItem("Sulfuras, Hand of Ragnaros", 0, 80));
         items.add(new TakeItBeforeTooLateItem("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-        items.add(new ItemHolder(new Item("Conjured Mana Cake", 3, 6)));
+        items.add(new ConjuredItem(new NormalItem("Conjured Mana Cake", 3, 6)));
 
         updateQuality();
     }
