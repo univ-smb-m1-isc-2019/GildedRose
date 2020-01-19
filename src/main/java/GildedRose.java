175d3c6d9b88e71c578008270ce90da124155d2f
Olivier LESAGE <olivier.lesage@etu.univ-savoie>
2020-01-19 23:38:53 +0100

    Ajout d'objet de type "LegendaryItem" dans le main.

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -17,7 +17,7 @@ public class GildedRose {
         items.add(new NormalItem("+5 Dexterity Vest", 10, 20));
         items.add(new ItemHolder(new Item("Aged Brie", 2, 0)));
         items.add(new NormalItem("Elixir of the Mongoose", 5, 7));
-        items.add(new ItemHolder(new Item("Sulfuras, Hand of Ragnaros", 0, 80)));
+        items.add(new LegendaryItem("Sulfuras, Hand of Ragnaros", 0, 80));
         items.add(new ItemHolder(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)));
         items.add(new ItemHolder(new Item("Conjured Mana Cake", 3, 6)));
 
