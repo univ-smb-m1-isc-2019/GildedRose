4d0af53ab9431825606789a18d230258edcea0ca
TheoJugand <theosam@live.fr>
2020-01-19 20:26:59 +0000

    Ajout Test GildedRose + Ajout Classe

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        GildedRose gildedRose = new GildedRose();
+
+        gildedRose.updateQuality();
+    }
+}
