abeedff1c7e4433888be1cae42d66c1c988c6e7a
unknown <lucachapelle@gmail.com>
2020-01-20 18:23:20 +0100

    fin_test

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -86,5 +87,110 @@ public class GildedRoseTest {
         }
     }
 
+    @Test
+    public void quality_degrade__twice_item_conjured() {
+        GildedRose GR = new GildedRose();
+        Item conjured = GR.get("Conjured Mana Cake");
+
+        for (int i = 0; i < 100; i++) {
+            int originQualityConjured = conjured.getQuality();
+            GR.udapteQuality();
+
+
+            int actualQualityConjured = conjured.getQuality();
+            if (originQualityConjured - 2 >= 0 && conjured.getSellIn() >= 0) {
+                assertThat(originQualityConjured - 2).isEqualTo(actualQualityConjured);
+            }
+            else if (originQualityConjured - 4 >= 0 && conjured.getSellIn() < 0) {
+                assertThat(originQualityConjured - 4).isEqualTo(actualQualityConjured);
+            }
+        }
+    }
+    @Test
+    public void backstage_should_increase_of_quality() {
+        GildedRose GR = new GildedRose();
+
+        Item backstage = GR.get("Backstage passes to a TAFKAL80ETC concert");
+
+        for (int i = 0; i < 100; i++) {
+            int originQualityBackstage = backstage.getQuality();
+            GR.udapteQuality();
+
+            int actualQualityBackstage = backstage.getQuality();
+            int sellInBackstage = backstage.getSellIn();
+
+            if(actualQualityBackstage < 50) {
+                if (sellInBackstage < 10 && sellInBackstage >= 5) {
+                    assertThat(originQualityBackstage + 2).isEqualTo(actualQualityBackstage);
+                } else if (sellInBackstage < 5 && sellInBackstage >= 0) {
+                    assertThat(originQualityBackstage + 3).isEqualTo(actualQualityBackstage);
+
+                } else if (sellInBackstage < 0) {
+                    assertThat(actualQualityBackstage).isEqualTo(0);
+                } else {
+                    assertThat(originQualityBackstage + 1).isEqualTo(actualQualityBackstage);
+                }
+            }
+
+        }
+    }
+    @Test
+    public void quality_degrade_twice_when_sell_passed() {
+        GildedRose GR = new GildedRose();
+        List<AdvancedItem> maliste = GR.getItemList();
+        Item dexterity = GR.get("+5 Dexterity Vest");
+        Item elixir = GR.get("Elixir of the Mongoose");
+        Item conjured = GR.get("Conjured Mana Cake");
+
+        for (int i = 0; i < 100; i++) {
+            int originQualityDexterity = dexterity.getQuality();
+            int originQualityElixir = elixir.getQuality();
+            int originQualityConjured = conjured.getQuality();
+            GR.udapteQuality();
+            maliste.stream()
+                    .forEach(
+                            item -> {
+                                if (item.equals(elixir)  && elixir.getSellIn() < 0 && elixir.getQuality() != 0) {
+                                    assertThat(originQualityElixir - 2).isEqualTo(elixir.getQuality());
+                                }
+                                else if (item.equals(dexterity)  && dexterity.getSellIn() < 0 && dexterity.getQuality() != 0){
+                                    assertThat(originQualityDexterity - 2).isEqualTo(dexterity.getQuality());
+                                }
+                                else if (item.equals(conjured)   && conjured.getSellIn() < 0 && originQualityConjured - 4 >= 0){
+                                    assertThat(originQualityConjured - 4).isEqualTo(conjured.getQuality());
+                                }
+                            }
+                    );
+
+        }
+    }
+
+    @Test
+    public void quality_degrade_standard_item() {
+        GildedRose GR = new GildedRose();
+        List<AdvancedItem> maliste = GR.getItemList();
+        Item dexterity = GR.get("+5 Dexterity Vest");
+        Item elixir = GR.get("Elixir of the Mongoose");
+
+
+        for (int i = 0; i < 100; i++) {
+            int originQualityDexterity = dexterity.getQuality();
+            int originQualityElixir = elixir.getQuality();
+            GR.udapteQuality();
+            maliste.stream()
+                    .forEach(
+                            item -> {
+                                if (item.equals(elixir) && elixir.getSellIn() >= 0 && elixir.getQuality() != 0) {
+                                    assertThat(originQualityElixir - 1).isEqualTo(elixir.getQuality());
+                                }
+                                else if (item.equals(dexterity)  && dexterity.getSellIn() >= 0 && dexterity.getQuality() != 0){
+                                    assertThat(originQualityDexterity - 1).isEqualTo(dexterity.getQuality());
+                                }
+                            }
+                    );
+
+        }
+    }
 }
+
 
