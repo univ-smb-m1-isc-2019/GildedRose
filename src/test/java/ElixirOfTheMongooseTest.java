88bb27cf5cd7274d470d909a874291cd6075da0f
gjambet <guillaume.jambet@gmail.com>
2020-02-17 17:51:46 +0100

    added non regression testing for Elixir of the Mongoose

---
+++ src/test/java/ElixirOfTheMongooseTest.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ElixirOfTheMongooseTest {
+
+    @Test
+    public void nonRegression() {
+        GildedRose inn = new GildedRose();
+        Item item = new Item("Elixir of the Mongoose", 5, 7);
+
+        inn.add(item);
+        assertTrue(item.getQuality() == 7);
+        assertTrue(item.getSellIn() == 5);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 6);
+        assertTrue(item.getSellIn() == 4);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 5);
+        assertTrue(item.getSellIn() == 3);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 4);
+        assertTrue(item.getSellIn() == 2);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 3);
+        assertTrue(item.getSellIn() == 1);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 2);
+        assertTrue(item.getSellIn() == 0);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 0);
+        assertTrue(item.getSellIn() == -1);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 0);
+        assertTrue(item.getSellIn() == -2);
+    }
+
+
+}
