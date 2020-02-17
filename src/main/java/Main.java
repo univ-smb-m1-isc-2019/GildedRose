3308dadac3aae7cb209cef7359915fefe90aab0b
gjambet <guillaume.jambet@gmail.com>
2020-02-17 15:54:01 +0100

    extracted Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("OMGHAI!");
+
+        GildedRose inn = new GildedRose();
+        inn.add(new Item("+5 Dexterity Vest", 10, 20));
+        inn.add(new Item("Aged Brie", 2, 0));
+        inn.add(new Item("Elixir of the Mongoose", 5, 7));
+        inn.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+        inn.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+        inn.add(new Item("Conjured Mana Cake", 3, 6));
+        inn.updateQuality();
+
+    }
+
+
+}
--- src/main/java/gildedrose/GildedRose.java
+++ src/main/java/gildedrose/GildedRose.java
@@ -8,26 +8,14 @@ public class GildedRose {
 
     private final List<Item> items;
 
-    public static void main(String[] args) {
-
-        System.out.println("OMGHAI!");
-        GildedRose inn = new GildedRose();
-        inn.add(new Item("+5 Dexterity Vest", 10, 20));
-        inn.add(new Item("Aged Brie", 2, 0));
-        inn.add(new Item("Elixir of the Mongoose", 5, 7));
-        inn.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-        inn.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-        inn.add(new Item("Conjured Mana Cake", 3, 6));
-
-        inn.updateQuality();
-    }
-
-    private void add(Item item) {
-        this.items.add(item);
-    }
 
     public GildedRose() {
-        items = new ArrayList<Item>();
+        items = new ArrayList<>();
+    }
+
+
+    void add(Item item) {
+        this.items.add(item);
     }
 
 
