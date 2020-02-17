b122e4c745d88016eb66f2d8af9d39fdfa85b27c
gjambet <guillaume.jambet@gmail.com>
2020-02-17 16:10:26 +0100

    added non regression testing in Dexterity Vest

---
+++ src/test/java/DexterityVestTest.java
@@ -0,0 +1,82 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DexterityVestTest {
+
+    @Test
+    public void nonRegression() {
+        GildedRose inn = new GildedRose();
+        Item item = new Item("+5 Dexterity Vest", 10, 20);
+
+        inn.add(item);
+        assertTrue(item.getQuality() == 20);
+        assertTrue(item.getSellIn() == 10);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 19);
+        assertTrue(item.getSellIn() == 9);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 18);
+        assertTrue(item.getSellIn() == 8);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 17);
+        assertTrue(item.getSellIn() == 7);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 16);
+        assertTrue(item.getSellIn() == 6);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 15);
+        assertTrue(item.getSellIn() == 5);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 14);
+        assertTrue(item.getSellIn() == 4);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 13);
+        assertTrue(item.getSellIn() == 3);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 12);
+        assertTrue(item.getSellIn() == 2);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 11);
+        assertTrue(item.getSellIn() == 1);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 10);
+        assertTrue(item.getSellIn() == 0);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 8);
+        assertTrue(item.getSellIn() == -1);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 6);
+        assertTrue(item.getSellIn() == -2);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 4);
+        assertTrue(item.getSellIn() == -3);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 2);
+        assertTrue(item.getSellIn() == -4);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 0);
+        assertTrue(item.getSellIn() == -5);
+
+        inn.updateQuality();
+        assertTrue(item.getQuality() == 0);
+        assertTrue(item.getSellIn() == -6);
+    }
+
+
+}
