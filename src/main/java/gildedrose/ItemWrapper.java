e420b0988938071e414e6401169c111fc0a80e83
gjambet <guillaume.jambet@gmail.com>
2020-03-02 14:46:15 +0100

    moved updateQuality to wrapper

--- src/main/java/gildedrose/ItemWrapper.java
+++ src/main/java/gildedrose/ItemWrapper.java
@@ -31,4 +31,84 @@ public class ItemWrapper {
     public void setQuality(int quality) {
         item.setQuality(quality);
     }
+
+    private boolean isBackstagePass(Item item) {
+        return "Backstage passes to a TAFKAL80ETC concert".equals(item.getName());
+    }
+
+    private boolean isAgedBrie(Item item) {
+        return "Aged Brie".equals(item.getName());
+    }
+
+    private boolean isSulfuras(Item item) {
+        return "Sulfuras, Hand of Ragnaros".equals(item.getName());
+    }
+
+    private void decrementSellIn(Item item) {
+        item.setSellIn(item.getSellIn() - 1);
+    }
+
+    private void decrementQuality(Item item) {
+        item.setQuality(item.getQuality() - 1);
+    }
+
+    private void incrementQuality(Item item) {
+        item.setQuality(item.getQuality() + 1);
+    }
+
+
+    public void updateQuality() {
+
+        if ((isAgedBrie(item)) || isBackstagePass(item)) {
+            if (item.getQuality() < 50) {
+                incrementQuality(item);
+
+                if (isBackstagePass(item)) {
+                    if (item.getSellIn() < 11) {
+                        if (item.getQuality() < 50) {
+                            incrementQuality(item);
+                        }
+                    }
+
+                    if (item.getSellIn() < 6) {
+                        if (item.getQuality() < 50) {
+                            incrementQuality(item);
+                        }
+                    }
+                }
+            }
+        } else {
+            if (item.getQuality() > 0) {
+                if (isSulfuras(item)) {
+                } else {
+                    decrementQuality(item);
+                }
+            }
+        }
+
+        if (isSulfuras(item)) {
+        } else {
+            decrementSellIn(item);
+        }
+
+        if (item.getSellIn() < 0) {
+            if (isAgedBrie(item)) {
+                if (item.getQuality() < 50) {
+                    incrementQuality(item);
+                }
+            } else {
+                if (isBackstagePass(item)) {
+                    item.setQuality(0);
+                } else {
+                    if (item.getQuality() > 0) {
+                        if (isSulfuras(item)) {
+                            return;
+                        }
+                        decrementQuality(item);
+                    }
+                }
+            }
+        }
+    }
+
 }
