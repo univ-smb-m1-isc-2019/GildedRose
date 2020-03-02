63caedef541bd61ff5ac432a79e6b4a980cfe334
gjambet <guillaume.jambet@gmail.com>
2020-03-02 14:10:23 +0100

    simplified expression

--- src/main/java/gildedrose/GildedRose.java
+++ src/main/java/gildedrose/GildedRose.java
@@ -62,7 +62,7 @@ public class GildedRose {
                             }
                         }
                     } else {
-                        item.setQuality(item.getQuality() - item.getQuality());
+                        item.setQuality(0);
                     }
                 } else {
                     if (item.getQuality() < 50) {
